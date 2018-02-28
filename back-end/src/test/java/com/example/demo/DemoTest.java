b8a37f6984ca756b6f2248efc9eaa404fbf2b59d
Marcos <marcos.kafok@gmail.com>
2018-02-28 19:34:50 +0100

    Solucion definitiva

--- back-end/src/test/java/com/example/demo/DemoTest.java
+++ back-end/src/test/java/com/example/demo/DemoTest.java
@@ -1,13 +1,25 @@
 package com.example.demo;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.domain.Game;
+import com.example.demo.services.GameService;
+
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class DemoTest {
- 
+
+	@Autowired
+	private GameService gameService;
+	
     @Test
-    public void test() {
-    	
+    public void create() {
+    	Game g = gameService.create("nombre");
+    	Game g2 = gameService.findOne(g.getId());
+    	Assert.assertEquals(g2.getId(), g.getId());
     }
 }
