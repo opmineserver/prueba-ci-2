92be046b429ffbe5a21aa6673898cd765526d5d1
Marcos <marcos.kafok@gmail.com>
2018-02-28 18:06:07 +0100

    Maquinas virtuales y travis flama

--- VMs/MySQL/vm/install.sh
+++ VMs/MySQL/vm/install.sh
@@ -3,4 +3,3 @@ sudo sh index.html
 
 sudo docker pull mysql/mysql-server:5.7
 sudo docker run --name=mysql -p 3306:3306 -e MYSQL_DATABASE='dev_db' -e MYSQL_ROOT_HOST='%' -e MYSQL_ROOT_PASSWORD='1234' -d mysql/mysql-server:5.7
-sudo docker stop mysql
--- VMs/PostGreSQL/vm/install.sh
+++ VMs/PostGreSQL/vm/install.sh
@@ -2,5 +2,4 @@ wget https://get.docker.com/
 sudo sh index.html
 
 sudo docker pull docker pull postgres:10.2
-sudo docker run --name=postgres -p 5432:5432 -e POSTGRES_DB='dev_db' -e POSTGRES_USER='root' -e POSTGRES_PASSWORD='1234' -d mysql/mysql-server:5.7
-sudo docker stop postgres
+sudo docker run --name=postgres -p 5432:5432 -e POSTGRES_DB='dev_db' -e POSTGRES_USER='root' -e POSTGRES_PASSWORD='1234' -d postgres:10.2
