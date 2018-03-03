056e49de62059237b40b1ce698394df54dcd7451
Marcos <marcos.kafok@gmail.com>
2018-03-03 21:56:26 +0100

    Rula cabron

--- back-end/src/main/java/com/example/demo/SecurityConfig.java
+++ back-end/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
