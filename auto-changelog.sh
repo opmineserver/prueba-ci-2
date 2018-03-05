9f8cf928844341aec7a2ee1c5a4e7e5ce15919c2
opmineserver <opmineserver@gmail.com>
2018-03-05 18:52:46 +0100

    Fix name

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -4,7 +4,9 @@ if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	echo "===================================================================SIIIIIIIIIIIIIIIIIIIII====================================================="
 	git checkout gh-pages
 	mkdir changelogs
-	auto-changelog -o changelogs/CHANGELOG-$APP_VERSION.md -l false
+	cd changelogs
+	auto-changelog -o CHANGELOG-$APP_VERSION.md -l false
+	cd ..
 	git add .
 	git commit -m "Changelog v$APP_VERSION"
 	git push "https://$GIT_PASS@${GIT_REPO}" gh-pages > /dev/null 2>&1
