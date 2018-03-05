4f6233458911a4331da32e423cfef4aa5faf8a42
Marcos <marcos.kafok@gmail.com>
2018-03-06 00:43:05 +0100

    Origin

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -10,6 +10,6 @@ if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	cd ..
 	git add .
 	git commit -m "Changelog v$APP_VERSION"
-	git push "https://$GIT_USER:$GIT_PASS@github.com/$GIT_REPO.git" gh-pages
+	git push origin gh-pages
 	git checkout master
 fi
