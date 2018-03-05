36db704a65f19ba8e3f31a74c98d46e810c7f1c6
opmineserver <opmineserver@gmail.com>
2018-03-05 18:10:16 +0100

    Otra ve fix

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ $(git tag -l "$APP_VERSION") ]; then
+if [ $(git tag -l "$APP_VERSION") != $APP_VERSION ]; then
 	git checkout gh-pages
 	auto-changelog -o changelogs/CHANGELOG-$APP_VERSION.md -l false
+	git add .
+	git commit -m "Changelog v$APP_VERSION"
 	git push "https://$GIT_PASS@${GIT_REPO}" gh-pages > /dev/null 2>&1
 	git checkout master
 	git tag -a v$APP_VERSION -m "v$APP_VERSION"
