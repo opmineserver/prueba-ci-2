336ec9c98765190bb0795f6819fe12dc5b868459
Marcos <marcos.kafok@gmail.com>
2018-03-06 23:38:42 +0100

    Habeeeeee!!

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export $DO_RELEASE = "false"
 if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	cd ..
 	git clone --depth=2 --branch=gh-pages https://github.com/$GIT_USER/$GIT_REPO.git repo
@@ -12,6 +13,7 @@ if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	git commit -m "Changelog $APP_VERSION"
 	git push "https://$GIT_KEY@github.com/$GIT_USER/$GIT_REPO.git"
 	cd ..
-	ls -a
 	cd $GIT_REPO
+	git tag -a $APP_VERSION -m "$APP_VERSION"
+	export $DO_RELEASE = "true"
 fi
