48b0bc00fddf571aac406479dd11245cfbae23e7
Marcos <marcos.kafok@gmail.com>
2018-03-06 22:07:36 +0100

    Por fin depliegaaa!!

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -2,14 +2,16 @@
 
 if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	echo "===================================================================SIIIIIIIIIIIIIIIIIIIII====================================================="
-	git fetch origin gh-pages
-	git checkout -b gh-pages HEAD
+	cd ..
+	git clone --depth=2 --branch=gh-pages https://github.com/$GIT_REPO.git repo
+	cd repo
 	mkdir changelogs
 	cd changelogs
 	auto-changelog -o CHANGELOG-$APP_VERSION.md -l false
 	cd ..
 	git add .
 	git commit -m "Changelog v$APP_VERSION"
-	git push origin gh-pages
-	git checkout master
+	git push "https://$GIT_KEY@github.com/$GIT_REPO.git"
+	cd ..
+	cd $GIT_REPO
 fi
