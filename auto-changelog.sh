e7ba238886387de9a29736383855a15d8e1d9097
Marcos <marcos.kafok@gmail.com>
2018-03-06 23:43:57 +0100

    Vengaaa

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-export $DO_RELEASE = "false"
 if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	cd ..
 	git clone --depth=2 --branch=gh-pages https://github.com/$GIT_USER/$GIT_REPO.git repo
@@ -15,5 +14,5 @@ if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	cd ..
 	cd $GIT_REPO
 	git tag -a $APP_VERSION -m "$APP_VERSION"
-	export $DO_RELEASE = "true"
+	export $DO_RELEASE="true"
 fi
