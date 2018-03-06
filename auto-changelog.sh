f011fe15890b71a020d9974876f56b1e0bb6ae71
Marcos <marcos.kafok@gmail.com>
2018-03-06 22:16:19 +0100

    Amo a ve

--- auto-changelog.sh
+++ auto-changelog.sh
@@ -3,7 +3,7 @@
 if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	echo "===================================================================SIIIIIIIIIIIIIIIIIIIII====================================================="
 	cd ..
-	git clone --depth=2 --branch=gh-pages https://github.com/$GIT_REPO.git repo
+	git clone --depth=2 --branch=gh-pages https://github.com/$GIT_USER/$GIT_REPO.git repo
 	cd repo
 	mkdir changelogs
 	cd changelogs
@@ -11,7 +11,7 @@ if [[ $(git tag -l "$APP_VERSION") != $APP_VERSION ]]; then
 	cd ..
 	git add .
 	git commit -m "Changelog v$APP_VERSION"
-	git push "https://$GIT_KEY@github.com/$GIT_REPO.git"
+	git push "https://$GIT_KEY@github.com/$GIT_USER/$GIT_REPO.git"
 	cd ..
 	ls -a
 	cd $GIT_REPO
